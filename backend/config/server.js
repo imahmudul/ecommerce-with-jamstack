761ebe51830ba440d808cbdc0c1889d90a4dd87a
Mahmudul Hasan <mahmudul.online@gmail.com>
2021-08-20 22:06:54 +0600

    Added Strapi, Gatsby with GraphQL support. Working on Layout Design

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3ef3342207da5dcc66e5e73cb0bc0649'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'def0a74d-df55-4fe6-b08a-6e009a0f9ddf'
+};
